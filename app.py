7aaa609f65ffe9a97a509b1a94ceed71d9ab0911
Bertrand <142926167+bertrandrusa@users.noreply.github.com>
2024-09-10 02:56:32 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,168 +1,151 @@
 import dash
 from dash import dcc
 from dash import html
+from dash.dependencies import Output, Input, State
+from dash.exceptions import PreventUpdate
 from datetime import datetime as dt
 import yfinance as yf
-from dash.dependencies import Input, Output, State
-from dash.exceptions import PreventUpdate
 import pandas as pd
 import plotly.graph_objs as go
 import plotly.express as px
+
 # model
 from model import prediction
 from sklearn.svm import SVR
 
-
-def get_stock_price_fig(df):
-
-    fig = px.line(df,
-                  x="Date",
-                  y=["Close", "Open"],
-                  title="Closing and Openning Price vs Date")
-
-    return fig
-
-
-def get_more(df):
-    df['EWA_20'] = df['Close'].ewm(span=20, adjust=False).mean()
-    fig = px.scatter(df,
-                     x="Date",
-                     y="EWA_20",
-                     title="Exponential Moving Average vs Date")
-    fig.update_traces(mode='lines+markers')
-    return fig
-
-
-app = dash.Dash(
-    __name__,
-    external_stylesheets=[
-        "https://fonts.googleapis.com/css2?family=Roboto&display=swap"
-    ])
+app = dash.Dash(__name__, external_stylesheets=['assets\styles.css'])
 server = app.server
-# html layout of site
-app.layout = html.Div(
+
+# Define the layout components
+
+# Navigation component
+item1 = html.Div(
+    [
+        html.P("Welcome to the Stock Dash App!", className="start"),
+
+        html.Div([
+            # stock code input
+            dcc.Input(id='stock-code', type='text', placeholder='Enter stock code'),
+            html.Button('Submit', id='submit-button')
+        ], className="stock-input"),
+
+        html.Div([
+            # Date range picker input
+            dcc.DatePickerRange(
+                id='date-range', start_date=dt(2020, 1, 1).date(), end_date=dt.now().date(), className='date-input')
+        ]),
+        html.Div([
+            # Stock price button
+            html.Button('Get Stock Price', id='stock-price-button'),
+
+            # Indicators button
+            html.Button('Get Indicators', id='indicators-button'),
+
+            # Number of days of forecast input
+            dcc.Input(id='forecast-days', type='number', placeholder='Enter number of days'),
+
+            # Forecast button
+            html.Button('Get Forecast', id='forecast-button')
+        ], className="selectors")
+    ],
+    className="nav"
+)
+
+# Content component
+item2 = html.Div(
     [
         html.Div(
             [
-                # Navigation
-                html.P("Welcome to the Stock Dash App!", className="start"),
-                html.Div([
-                    html.P("Input stock code: "),
-                    html.Div([
-                        dcc.Input(id="dropdown_tickers", type="text"),
-                        html.Button("Submit", id='submit'),
-                    ],
-                             className="form")
-                ],
-                         className="input-place"),
-                html.Div([
-                    dcc.DatePickerRange(id='my-date-picker-range',
-                                        min_date_allowed=dt(1995, 8, 5),
-                                        max_date_allowed=dt.now(),
-                                        initial_visible_month=dt.now(),
-                                        end_date=dt.now().date()),
-                ],
-                         className="date"),
-                html.Div([
-                    html.Button(
-                        "Stock Price", className="stock-btn", id="stock"),
-                    html.Button("Indicators",
-                                className="indicators-btn",
-                                id="indicators"),
-                    dcc.Input(id="n_days",
-                              type="text",
-                              placeholder="number of days"),
-                    html.Button(
-                        "Forecast", className="forecast-btn", id="forecast")
-                ],
-                         className="buttons"),
-                # here
+                html.Img(id='logo', className='logo'),
+                html.H1(id='company-name', className='company-name')
             ],
-            className="nav"),
-
-        # content
-        html.Div(
-            [
-                html.Div(
-                    [  # header
-                        html.Img(id="logo"),
-                        html.P(id="ticker")
-                    ],
-                    className="header"),
-                html.Div(id="description", className="decription_ticker"),
-                html.Div([], id="graphs-content"),
-                html.Div([], id="main-content"),
-                html.Div([], id="forecast-content")
-            ],
-            className="content"),
+            className="header"),
+        html.Div(id="description"),
+        html.Div([], id="graphs-content"),
+        html.Div([], id="main-content"),
+        html.Div([], id="forecast-content")
     ],
-    className="container")
-
-
-# callback for company info
-@app.callback([
-    Output("description", "children"),
-    Output("logo", "src"),
-    Output("ticker", "children"),
-    Output("stock", "n_clicks"),
-    Output("indicators", "n_clicks"),
-    Output("forecast", "n_clicks")
-], [Input("submit", "n_clicks")], 
-   [State("dropdown_tickers", "value")]
+    className="content"
 )
-def update_data(n, val):  # inpur parameter(s)
-    if n == None:
-        return "Hey there! Please enter a legitimate stock code to get details.", "https://melmagazine.com/wp-content/uploads/2019/07/Screen-Shot-2019-07-31-at-5.47.12-PM.png", "Stonks", None, None, None
-        # raise PreventUpdate
+
+# Set the layout
+app.layout = html.Div(className='container', children=[item1, item2])
+
+# Callbacks
+
+# Callback to update the data based on the submitted stock code
+@app.callback(
+    [
+        Output("description", "children"),
+        Output("logo", "src"),
+        Output("company-name", "children"),
+        Output("stock-price-button", "n_clicks"),
+        Output("indicators-button", "n_clicks"),
+        Output("forecast-button", "n_clicks")
+    ],
+    [Input("submit-button", "n_clicks")],
+    [State("stock-code", "value")]
+)
+def update_data(n, val):
+    if n is None:
+        return None, None, None, None, None, None
     else:
-        if val == None:
+        if val is None:
             raise PreventUpdate
         else:
             ticker = yf.Ticker(val)
             inf = ticker.info
-            df = pd.DataFrame().from_dict(inf, orient="index").T
-            df[['logo_url', 'shortName', 'longBusinessSummary']]
-            return df['longBusinessSummary'].values[0], df['logo_url'].values[0], df['shortName'].values[0], None, None, None
+            if 'logo_url' not in inf:
+                return None, None, None, None, None, None
+            else:
+                name = inf['longName']
+                logo_url = inf['logo_url']
+                description = inf['longBusinessSummary']
+                return description, logo_url, name, None, None, None
 
 
-# callback for stocks graphs
-@app.callback([
-    Output("graphs-content", "children"),
-], [
-    Input("stock", "n_clicks"),
-    Input('my-date-picker-range', 'start_date'),
-    Input('my-date-picker-range', 'end_date')
-], [State("dropdown_tickers", "value")])
+# Callback for displaying stock price graphs
+@app.callback(
+    [Output("graphs-content", "children")],
+    [
+        Input("stock-price-button", "n_clicks"),
+        Input('date-range', 'start_date'),
+        Input('date-range', 'end_date')
+    ],
+    [State("stock-code", "value")]
+)
 def stock_price(n, start_date, end_date, val):
-    if n == None:
+    if n is None:
         return [""]
-        #raise PreventUpdate
-    if val == None:
+    if val is None:
         raise PreventUpdate
     else:
-        if start_date != None:
+        if start_date is not None:
             df = yf.download(val, str(start_date), str(end_date))
         else:
             df = yf.download(val)
 
     df.reset_index(inplace=True)
-    fig = get_stock_price_fig(df)
+    fig = px.line(df, x="Date", y=["Close", "Open"], title="Closing and Opening Price vs Date")
     return [dcc.Graph(figure=fig)]
 
 
-# callback for indicators
-@app.callback([Output("main-content", "children")], [
-    Input("indicators", "n_clicks"),
-    Input('my-date-picker-range', 'start_date'),
-    Input('my-date-picker-range', 'end_date')
-], [State("dropdown_tickers", "value")])
+# Callback for displaying indicators
+@app.callback(
+    [Output("main-content", "children")],
+    [
+        Input("indicators-button", "n_clicks"),
+        Input('date-range', 'start_date'),
+        Input('date-range', 'end_date')
+    ],
+    [State("stock-code", "value")]
+)
 def indicators(n, start_date, end_date, val):
-    if n == None:
+    if n is None:
         return [""]
-    if val == None:
+    if val is None:
         return [""]
-
-    if start_date == None:
+    if start_date is None:
         df_more = yf.download(val)
     else:
         df_more = yf.download(val, str(start_date), str(end_date))
@@ -172,15 +155,24 @@ def indicators(n, start_date, end_date, val):
     return [dcc.Graph(figure=fig)]
 
 
-# callback for forecast
-@app.callback([Output("forecast-content", "children")],
-              [Input("forecast", "n_clicks")],
-              [State("n_days", "value"),
-               State("dropdown_tickers", "value")])
+def get_more(df):
+    df['EWA_20'] = df['Close'].ewm(span=20, adjust=False).mean()
+    fig = px.scatter(df, x="Date", y="EWA_20", title="Exponential Moving Average vs Date")
+    fig.update_traces(mode='lines+markers')
+    return fig
+
+
+# Callback for displaying forecast
+@app.callback(
+    [Output("forecast-content", "children")],
+    [Input("forecast-button", "n_clicks")],
+    [State("forecast-days", "value"),
+     State("stock-code", "value")]
+)
 def forecast(n, n_days, val):
-    if n == None:
+    if n is None:
         return [""]
-    if val == None:
+    if val is None:
         raise PreventUpdate
     fig = prediction(val, int(n_days) + 1)
     return [dcc.Graph(figure=fig)]
